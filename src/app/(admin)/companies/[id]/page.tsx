b455282dafdaf80fb2b05d9cd909b75d1ebab885
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-12-21 14:47:40 +0200

    add hooks and Link for navigation

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from '@/app/components/header';
 
-export interface PageProps {
-  params: { id: string };
-}
-
-export default async function Page({ params }: PageProps) {
-  const { id } = await params;
-
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   return (
     <>
-      <Header>Company ({id})</Header>
+      <Header>Company ({(await params).id})</Header>
     </>
   );
 }
